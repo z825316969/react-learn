dbfd4506c6e59189862b96ce837aaaafeba9f044
赵晓晨 <825316969@qq.com>
2021-08-20 20:02:35 +0800

    update(formily): formily试用

--- src/Formilyjs.jsx
+++ src/Formilyjs.jsx
@@ -1,10 +1,39 @@
-import React, { useState, useEffect } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react'
+import { createForm } from '@formily/core'
+import { FormProvider, FormConsumer, Field } from '@formily/react'
+import { FormItem, FormLayout, Input, FormButtonGroup, Submit } from '@formily/antd'
 
-Formilyjs.propTypes = {}
+const form = createForm()
 
-Formilyjs.defaultProps = {}
-
-export default function Formilyjs(props) {
-  return <div>123</div>
+export default () => {
+  return (
+    <FormProvider form={form}>
+      <FormLayout layout="vertical">
+        <Field
+          name="input"
+          title="输入框"
+          required
+          initialValue="Hello world"
+          decorator={[FormItem]}
+          component={[Input]}
+        />
+      </FormLayout>
+      <FormConsumer>
+        {() => (
+          <div
+            style={{
+              marginBottom: 20,
+              padding: 5,
+              border: '1px dashed #666',
+            }}
+          >
+            实时响应：{form.values.input}
+          </div>
+        )}
+      </FormConsumer>
+      <FormButtonGroup>
+        <Submit onSubmit={console.log}>提交</Submit>
+      </FormButtonGroup>
+    </FormProvider>
+  )
 }
